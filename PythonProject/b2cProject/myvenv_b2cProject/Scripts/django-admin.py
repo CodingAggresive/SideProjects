1aa98e4812bf44a93baa1160f7a75bbc260392b6
CodingAggresive <rfeng@smsassist.com>
2019-12-12 15:56:56 -0600

    Add new SideProjects folder

---
+++ PythonProject/b2cProject/myvenv_b2cProject/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\work\sideprojects\pythonproject\b2cproject\myvenv_b2cproject\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
