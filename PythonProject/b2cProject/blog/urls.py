d1413118b5442efa8d4c71721291c0d4685ad126
CodingAgressive <missshirly.career@gmail.com>
2019-12-23 17:18:57 -0600

    extended application

--- PythonProject/b2cProject/blog/urls.py
+++ PythonProject/b2cProject/blog/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    #path(url,view,template django template tag url name)
+    #In blog/urls.py we created a URL rule named post_detail that refers to a view called views.post_detail. 
+    # This means that Django will be expecting a view function called post_detail inside blog/views.py.
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
