efec1e5e2c7517fcb0d8014477e58399f1db6b40
CodingAgressive <missshirly.career@gmail.com>
2020-02-03 17:30:22 -0600

    CSharpProject and uncompleted PythonProject

---
+++ CSharpProject/WebScraping/WebCrawlerAndScraping/WebCrawlerAndScraping/Controllers/SampleDataController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebCrawlerAndScraping.Controllers
+{
+    public class PersonData
+    {
+        public int ID { get; set; }
+        public string PersonName { get; set; }
+        public int Nationality { get; set; }
+
+        public PersonData(int id, int nationality, string Name)
+        {
+            ID = id;
+            PersonName = Name;
+            Nationality = nationality;
+        }
+    }
+
+    public class Country
+    {
+        public int ID { get; set; }
+        public string CountryName { get; set; }
+
+        public Country(int id, string Name)
+        {
+            ID = id;
+            CountryName = Name;
+        }
+    }
+
+    public class SampleData
+    {
+        public List<Country> Countries;
+        public List<PersonData> People;
+
+        public SampleData()
+        {
+            Countries = new List<Country>();
+            People = new List<PersonData>();
+
+            Countries.Add(new Country(1, "United Kingdom"));
+            Countries.Add(new Country(2, "United States"));
+            Countries.Add(new Country(3, "Republic of Ireland"));
+            Countries.Add(new Country(4, "India"));
+            People.Add(new PersonData(1, 1, "AJSON"));
+            People.Add(new PersonData(2, 2, "Fred"));
+            People.Add(new PersonData(3, 2, "Mary"));
+
+        }
+    }
+
+    public class SampleDataController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- PythonProject/b2cProject/online_shop/models.py
+++ PythonProject/b2cProject/online_shop/models.py
@@ -1,39 +1,57 @@
 from django.db import models
 
 # Create your models here.
-from django.conf import settings  #????
+#????
+from django.conf import settings  
 from django.db import models
 from django.utils import timezone
 
 class User(models.Model):
-    UserName = models.CharField(max_length=200)
-    FirstName = models.CharField(max_length=100)
-    MiddleName = models.CharField(max_length=100)
+    UserName = models.CharField(max_length=200,default='')
+    FirstName = models.CharField(max_length=100,default ='')
+    '''MiddleName = models.CharField(max_length=100)
     LastName = models.CharField(max_length=100)
     Password = models.CharField(max_length=200)
     Address = models.CharField(max_length=200)
     Email = models.CharField(max_length=100)
     CellPhoneNumber = models.CharField(max_length=50)
     CreateDate = models.DateTimeField(default = timezone.now)
-    Balance = models.DecimalField(max_digits=20,decimal_places=2)
+    Balance = models.DecimalField(max_digits=20,decimal_places=2)'''
+    
+    def __init__(self,user=User()):
+         self.user = user
+
+    def get_userinfo_by_id(self,id):
+        self.user.objects.get(id = id)
+        print(user)
+
+    def add_user(self,user=User()):
+        self.user.objects.add(User(){})
+    
+    def delete_user(self,id):
+        
+
+    def __str__(self):
+        return self.UserName
+
+    
+        
+        
     
 
-
-class Order(models.Model):
-    ItemId = models.IntegerField(max_length=12)
-    #author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    Amount = models.DecimalField(max_digits=20,decimal_places=2)
-    Quality = models.DecimalField(max_digits=20,decimal_places=2)
-    Buyer = models.CharField(max_length=12)
-    #author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    CreateDate = models.DateTimeField(default = timezone.now)
-
-
 class Item(models.Model):
-    EnglishName = models.CharField(max_length=200)
-    ChineseName = models.CharField(max_length=200)
-    MarketPrice = models.DecimalField(max_digits=20,decimal_places=2)
-    MarketDate = models.DateTimeField(default = timezone.now)
-    CreateDate = models.DateTimeField(default = timezone.now)
-    Seller = models.CharField(max_length=12)
-    #author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    EnglishName = models.CharField(max_length=200,blank=True)
+    '''ChineseName = models.CharField(max_length=200,blank=True)
+    MarketPrice = models.DecimalField(max_digits=20,decimal_places=2,blank=True)
+    MarketDate = models.DateTimeField(default = timezone.now,blank=True)
+    CreateDate = models.DateTimeField(default = timezone.now,blank=True)
+    Seller = models.ForeignKey(User,on_delete=models.DO_NOTHING,blank=True)'''
+
+
+    
+class Order(models.Model):
+    #ItemId = models.ForeignKey(Item,on_delete=models.DO_NOTHING)
+    Amount = models.DecimalField(max_digits=20,decimal_places=2,default=0.0)
+    '''Quality = models.DecimalField(max_digits=20,decimal_places=2)
+    Buyer = models.ForeignKey(User,on_delete=models.DO_NOTHING)
+    CreateDate = models.DateTimeField(default = timezone.now)'''
